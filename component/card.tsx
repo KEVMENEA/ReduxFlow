7b2af3aa05efbace288253e956e8bdeb83d6acd4
KEVMENEA <meneakev@gmail.com>
2026-03-06 11:28:16 +0700

    Redux Flow

---
+++ component/card.tsx
@@ -0,0 +1,16 @@
+import { counterSlice, decrease, increase } from "@/lib/features/counter/counterSlice"
+import { useAppSelector } from "@/lib/hooks"
+import { useDispatch } from "react-redux"
+
+export default function Card() {
+    const count = useAppSelector ((state) => state.counter.value)
+    const dispatch = useDispatch();
+
+    return (
+        <div className="flex h-screen items-center justify-center">
+             <h2>Calling Global state {count} </h2>
+            <button onClick={() => dispatch(increase())}> Increase </button>
+            <button onClick={() => dispatch(decrease())}> Decrease  </button>
+        </div>
+    )
+}
